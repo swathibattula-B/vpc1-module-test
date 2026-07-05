34eb284630f066f44245cb3a77a44393b9f5c518
swathi <battulaswathi15407@gmail.com>
2026-07-05 18:19:37 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "project_name" {
+    type = string
+    default = "roboshop"
+}
+
+variable "env" {
+    type = string
+    default = "dev"
+}
+
--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,12 @@
 module "vpc" {
     source = "git::https://github.com/swathibattula-B/terraform-vpc.git?ref=main"
+    project = var.project_name
+    environment = var.env
+    vpc_tags = {
+        
+        Name = "${var.project_name}-${var.env}"
+        
+    }
     
+
 }    
