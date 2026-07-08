b87deda2f5cac68ff5b55ede5bcf4eee439df8f5
swathi <battulaswathi15407@gmail.com>
2026-07-08 10:19:41 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,7 @@
     source = "git::https://github.com/swathibattula-B/terraform-vpc.git?ref=main"
     project = var.project
     environment =var.environment
+    is_peering_required = true
 
 
   }
